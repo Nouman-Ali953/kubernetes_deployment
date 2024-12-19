2b638abd4c3523f33d00081d4bfd18fb0513a2e8
Nouman-Ali953 <nauman.9533@gmail.com>
2024-12-19 13:00:19 +0500

    home page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>this is the app that is hosted on aws and it's using docker, docker images, docker containers, container orchestration, kubernetes, prometheous and grafana, Thanks for providing me the opportunity to deploy </div>
+    <div className='w-full h-screen flex justify-center items-center p-4 pl-16 pr-16'>
+    <p className='italic'>this is the app that is hosted on <span className='font-bold'>aws</span> and it's using <span className='block p-4 pl-0'><span className='font-bold '>docker</span>, <span className='font-bold '>docker images</span>, <span className='font-bold '>docker containers</span>, <span className='font-bold '>container orchestration</span>, <span className='font-bold '>kubernetes</span>, <span className='font-bold '>prometheous</span> and <span className='font-bold '>grafana ...</span><span className='not-italic'> 😊</span> </span> </p></div>
   )
 }
 
