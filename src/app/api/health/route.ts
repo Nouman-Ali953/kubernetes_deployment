1c88f356e878e95c74fb722438d5732fb11422bf
Nouman-Ali953 <nauman.9533@gmail.com>
2024-12-20 14:43:21 +0500

    health route

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -2,7 +2,10 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-    return NextResponse.json({ message: "Everything is healthy 👁️" });
+    return NextResponse.json(
+      { message: "Everything is healthy 👁️" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error occurred:", error);
     return NextResponse.json({ message: "Unhealthy state" }, { status: 500 });
