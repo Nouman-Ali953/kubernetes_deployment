0dbcbe6fe2c4846d56749601d76cebb2a0b6a990
Nouman-Ali953 <nauman.9533@gmail.com>
2024-12-24 13:38:35 +0500

    added a new page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center p-4 pl-12 pr-12">
+      <h2 className="font-bold text-lg">Hello! in the Dashboard 🎊🎉🎊</h2>
+    </div>
+  );
+};
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center p-4 pl-16 pr-16">
-      <p className="italic">
-        this is the app that is hosted on <span className="font-bold">aws</span>{" "}
-        and it's using{" "}
-        <span className="block p-4 pl-0">
-          <span className="font-bold">docker</span>,{" "}
-          <span className="font-bold">docker images</span>,{" "}
-          <span className="font-bold">docker containers</span>,{" "}
-          <span className="font-bold">container orchestration</span>,{" "}
-          <span className="font-bold">kubernetes</span>,{" "}
-          <span className="font-bold">prometheous</span> and{" "}
-          <span className="font-bold">grafana ..</span>
-          <span className="not-italic"> 😊</span>{" "}
-        </span>
-      </p>
+    <div className="w-full h-screen flex justify-center items-center p-4 pl-12 pr-12 flex-col ">
+      <div className="relative">
+        <p className="italic">
+          this is the app that is hosted on{" "}
+          <span className="font-bold">aws</span> and it's using{" "}
+          <span className="block p-4 pl-0">
+            <span className="font-bold">docker</span>,{" "}
+            <span className="font-bold">docker images</span>,{" "}
+            <span className="font-bold">docker containers</span>,{" "}
+            <span className="font-bold">container orchestration</span>,{" "}
+            <span className="font-bold">kubernetes</span>,{" "}
+            <span className="font-bold">prometheous</span> and{" "}
+            <span className="font-bold">grafana ..</span>
+            <span className="not-italic"> 😊</span>{" "}
+          </span>
+        </p>
+        <Link
+          href="/dashboard"
+          className="flex gap-2 items-center absolute top-[100%] left-[0] p-4 pt-[.3rem] pb-[.3rem] text-sm rounded-md bg-black text-white transition-all duration-200 group"
+        >
+          Go to Dashboard <MoveRight className="inline-block transform transition-transform group-hover:translate-x-2 duration-200" />
+        </Link>
+      </div>
     </div>
   );
 };
